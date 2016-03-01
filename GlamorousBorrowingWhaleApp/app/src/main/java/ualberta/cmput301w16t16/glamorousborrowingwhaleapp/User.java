dab153ce29fb88f6f65f60df4904d55750c43d73
mbaydala <baydala@ualberta.ca>
2016-02-29 17:46:43 -0700

    added User model class

---
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/User.java
@@ -0,0 +1,79 @@
+package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Martina on 16-02-29.
+ */
+public class User {
+    private String name;
+    private String emailAddress;
+    private String phoneNumber;
+    private Drawable picture;
+    private ItemList itemsBorrowing;
+    private ItemList itemsRenting;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Drawable getPicture() {
+        return picture;
+    }
+
+    public void setPicture(Drawable picture) {
+        this.picture = picture;
+    }
+
+    public ItemList getItemsBorrowing() {
+        return itemsBorrowing;
+    }
+
+    public void setItemsBorrowing(ItemList itemsBorrowing) {
+        this.itemsBorrowing = itemsBorrowing;
+    }
+
+    public ItemList getItemsRenting() {
+        return itemsRenting;
+    }
+
+    public void setItemsRenting(ItemList itemsRenting) {
+        this.itemsRenting = itemsRenting;
+    }
+
+    public void addItemBorrowing(Item item) {
+        itemsBorrowing.add(item);
+    }
+
+    public void removeItemBorrowing(Item item) {
+        itemsBorrowing.remove(item);
+    }
+
+    public void addItemRenting(Item item) {
+        itemsRenting.add(item);
+    }
+
+    public void removeItemRenting(Item item) {
+        itemsRenting.remove(item);
+    }
+}
