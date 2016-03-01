219f714a6fb4485ae4ee8deb2060220e6e8f0aa2
elmtree8 <elmtree8@gmail.com>
2016-02-29 17:22:28 -0700

    Added some buttons and fields to the first couple activities

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MainActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MainActivity.java
@@ -1,7 +1,10 @@
 package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button signUpButton = (Button) findViewById(R.id.signUpButton);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                Intent logIntent = new Intent(view.getContext(), SignUpActivity.class);
+                startActivity(logIntent);
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setResult(RESULT_OK);
+                Intent logIntent = new Intent(view.getContext(), SignInActivity.class);
+                startActivity(logIntent);
+            }
+        });
     }
 }
--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/SignInActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/SignInActivity.java
@@ -1,7 +1,10 @@
 package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
