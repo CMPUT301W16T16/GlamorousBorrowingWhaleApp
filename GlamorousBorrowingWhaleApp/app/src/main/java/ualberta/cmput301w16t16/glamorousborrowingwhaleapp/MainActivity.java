7bfd6b10ba822b02d82281dd23dd3ac84722fbe6
mbaydala <baydala@ualberta.ca>
2016-02-29 21:09:18 -0700

    added picture for sign up activity view and added username for sign in activity view

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MainActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MainActivity.java
@@ -30,5 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(logIntent);
             }
         });
+
+
     }
 }
--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/UserController.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/UserController.java
@@ -3,12 +3,14 @@ package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class UserController extends AppCompatActivity {
+
+    User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_user_controller);
     }
 
 
