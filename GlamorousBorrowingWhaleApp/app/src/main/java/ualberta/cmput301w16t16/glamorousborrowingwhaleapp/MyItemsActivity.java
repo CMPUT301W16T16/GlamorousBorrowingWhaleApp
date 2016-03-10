4a99378e9b3367361981fc176d006e4d3e8f826c
elmtree8 <elmtree8@gmail.com>
2016-03-10 15:59:53 -0700

    Tried to add some stuff (broke some stuff :\)

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MyItemsActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MyItemsActivity.java
@@ -1,7 +1,11 @@
 package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MyItemsActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MyItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_items);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), NewListingActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/NewListingActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/NewListingActivity.java
@@ -9,17 +9,33 @@ import android.view.View;
 import android.widget.EditText;
 
 public class NewListingActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // TODO: setting a picture
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_listing);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        EditText name = (EditText) findViewById(R.id.name);
         EditText owner = (EditText) findViewById(R.id.owner);
         EditText status = (EditText) findViewById(R.id.status);
+        EditText size = (EditText) findViewById(R.id.size);
+        EditText description = (EditText) findViewById(R.id.description);
+        status.setText("Available");
+        owner.setText("You");
 
-        status.setText("Status: Available");
-        owner.setText("Owner: You");
+        String nameStr = name.getText().toString();
+        String sizeStr = size.getText().toString();
+        String descStr = description.getText().toString();
+        BidList bids = new BidList();
+        User currOwner = UserController.getUser();
+        Item latestItem = new Item();
+        latestItem.setTitle(nameStr);
+        latestItem.setDescription(descStr);
+        latestItem.setSize(sizeStr);
+        latestItem.setAvailability(true);
+        latestItem.setBids(bids);
+        latestItem.setOwner(currOwner);
     }
 }
