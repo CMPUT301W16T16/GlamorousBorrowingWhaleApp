bccdb0aa0677314a28e0114cdb4de4e681339c52
AdamGualberta <agendron@ualberta.ca>
2016-03-14 13:47:34 -0600

    implemented listview from es pull, added (commented out) code to attempt to get the theirItem view working

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/SearchResultsActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/SearchResultsActivity.java
@@ -1,8 +1,14 @@
 package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -16,17 +22,44 @@ import android.widget.TextView;
 public class SearchResultsActivity extends AppCompatActivity {
 
     TextView tv;
+    ListView itemsListView;
+    ArrayAdapter<Item> adapter;
+    ItemList items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
         tv = (TextView) findViewById(R.id.textViewForTesting);
+        itemsListView = (ListView) findViewById(R.id.myItemsListView);
         setTitle("Search Results: All");
         // TODO ES CALL: get items to populate list
-        new ElasticSearch.elasticGetItems().execute(tv);
+        //adapter = new ArrayAdapter<Item>(SearchResultsActivity.this, R.layout.list_item, items.getItemList());
+        //itemsListView.setAdapter(adapter);
+        new ElasticSearch.elasticGetItems(getApplicationContext()).execute(itemsListView);
+
+        // I have no clue if the following is proper, it was Android Studio doing that autocomplete
+        // thing.
+        /* Doesn't work atm, IDK why yet.
+        itemsListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //This chunk is pretty neat. The ViewAdapter makes note of what is clicked in
+                //the dynamic list which is used to get the corresponding Item at that "position".
+                //Again leveraging the ItemController, we can set that Item as the current Item
+                //and send the user to the MyItemActivity with the current Item (the one
+                //that they selected!).
+                ItemController.setItem((Item) parent.getAdapter().getItem(position));
+                Toast.makeText(SearchResultsActivity.this, ItemController.getItem().getTitle(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(view.getContext(), TheirItemActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });*/
 
     }
+
+
 
 
 }
