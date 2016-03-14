2f1cb03f724e02ab20cd2891d5ffddbf0dbe402f
AdamGualberta <agendron@ualberta.ca>
2016-03-14 12:02:43 -0600

    doing the merge thing

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/UserController.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/UserController.java
@@ -2,13 +2,15 @@ package ualberta.cmput301w16t16.glamorousborrowingwhaleapp;
 
 import java.util.ArrayList;
 
+
 /**
  * Created by Martina on 16-03-10.
- * Holds the current user so it can be referenced throughout the app
+ * Holds the current + another user so it can be referenced throughout the app
  */
 //Is the controller for the user. Persistent throughout the user session.
 public class UserController {
     private static User user;
+    private static User otherUser;
 
     //TODO will we have this ask the server for a USER?
 
@@ -23,4 +25,15 @@ public class UserController {
     public static void setUser(User user) {
         UserController.user = user;
     }
+
+    public static User getOtherUser() {
+        return UserController.otherUser;
+    }
+
+    public static void setOtherUser(User otherUser) {
+        UserController.otherUser = otherUser;
+    }
+/*      otherUser.setName("Michelangelo Ferdinand");
+        otherUser.setEmailAddress("ilikepizza2321@aol.com");
+        otherUser.setPhoneNumber("18002672001");*/
 }
