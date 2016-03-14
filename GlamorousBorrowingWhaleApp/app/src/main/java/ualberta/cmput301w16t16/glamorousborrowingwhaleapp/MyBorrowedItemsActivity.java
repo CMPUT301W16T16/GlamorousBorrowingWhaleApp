059d99cd6393231fb9527a5e03e6ec57547dc8bc
andrewpaquette <apaquett@ualberta.ca>
2016-03-14 15:17:45 -0600

    minor change to Borrowed Items empty page

--- GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MyBorrowedItemsActivity.java
+++ GlamorousBorrowingWhaleApp/app/src/main/java/ualberta/cmput301w16t16/glamorousborrowingwhaleapp/MyBorrowedItemsActivity.java
@@ -4,6 +4,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 /**
  * This activity, much like MyItemsActivity, lists the items that the user is
@@ -21,6 +22,8 @@ public class MyBorrowedItemsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_borrowed_items);
-        setTitle("Items I've Borrowed");
+        setTitle("Items You've Borrowed");
+        TextView tv = (TextView) findViewById(R.id.borrowingList);
+        tv.setText("You have borrowed no items.");
     }
 }
